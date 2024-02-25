0830aa9c2f7927819ab7394298ce0e190b9cba6c
Hasir Ali <hasira804@gmail.com>
2024-02-25 21:22:04 +0530

    Hook Done

--- Foodinger/src/components/Body.jsx
+++ Foodinger/src/components/Body.jsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 function Body() {
   // scope of state variable is only in this component
   const [filteredResObj, setFilteredResObj] = useState(resObj);
+  // array destructuring⬆️
   const [z, setz] = useState(filteredResObj);
 
   return (
